d78ee9c140285191cf87ed9dcb28caf4d2e80e0c
Keren Stoller <keren123stoller@gmail.com>
2026-03-04 11:56:26 +0200

    first build

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "4rem" }}>
+      <h1>SKI App</h1>
+      <p>Welcome! The frontend is up and running.</p>
+    </div>
+  );
+}
+
+export default App;
