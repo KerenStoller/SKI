a34f51e6dcc9b82e174573a68d151717e3505966
Ido <idonew2@gmail.com>
2026-05-30 08:20:39 +0300

    Update database configuration

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,8 +1,13 @@
 # database.py
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.pool import NullPool
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+load_dotenv(BASE_DIR / ".env")
+
 
 # Load the secrets from the .env file
 load_dotenv(".env")
